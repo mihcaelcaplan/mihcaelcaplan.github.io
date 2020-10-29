02db03588c1c80a66a7f11a8809db077a6b410ee
mihcaelcaplan <michael_caplan@alumni.brown.edu>
2020-10-28 21:32:15 -0400

    switching to es5 to make backgorund darawing work

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ var win_length;
 var R1;
 var R2;
 
-class rect {
-    constructor (color){
+   function rect(color){
         
         this.color = color;
         this.right = getRandomInt(0,1);
@@ -17,26 +16,27 @@ class rect {
         this.height = getRandomInt(50, 200);
         
     }
-    draw = ()=>{
+
+    rect.prototype.draw = function(){
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
-    clear = ()=>{
+
+    rect.prototype.clear = function(){
         ctx.clearRect(this.x, this.y, this.width, this.height);
     }
 
-    moveV = (step)=>{
+    rect.prototype.moveV = function(step){
         this.clear()
         this.y += step;
         this.draw()
     }
 
-    moveH = (step)=>{
+    rect.prototype.moveH = function(step){
         this.clear()
         this.x += step;
         this.draw()
     }
-}
   
 
 //helpful functions 
@@ -47,7 +47,7 @@ function getRandomInt(min, max) {
 }
 
 //run when page loads 
-window.onload = ()=>{
+window.onload = function(){
     ctx = document.querySelector("canvas").getContext("2d");
     ctx.globalAlpha = 0.1;
 
@@ -55,22 +55,8 @@ window.onload = ()=>{
     win_width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
     win_height = window.innerHeight|| document.documentElement.clientHeight|| document.body.clientHeight;
     console.log(`window width: ${win_width} height: ${win_height}`)
-    R1 = new rect('green');
-    R2 = new rect('red');
+    var R1 = new rect('green');
+    var R2 = new rect('red');
     R1.draw();
     R2.draw();
 }
-
-// //interval funcs
-// setInterval(()=>{
-//     stepV = .1;
-//     stepH = 0.005;
-
-//     R1.moveV(stepV);
-
-//     // for(r in rect_l){
-//     //     r.moveV(stepV);
-//     //     r.moveH(stepH);
-//     // }
-
-// }, 33)
